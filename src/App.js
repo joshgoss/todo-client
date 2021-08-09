85e8b53f1d20637bff682641da1e2f96fe4b98b3
Joshua Goss <josh@joshgoss.com>
2021-08-09 12:28:12 -0400

    clear notifications on route change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import {
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
-import './App.scss';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import "./App.scss";
 
-import AppLayoutRoute from './layouts/AppLayout';
-import LoginLayoutRoute from './layouts/LoginLayout';
-import CreateAccountPage from './features/account/CreateAccountPage';
-import LoginPage from './features/account/LoginPage';
-import TodoPage from './features/todo/TodoPage';
+import AppLayoutRoute from "./layouts/AppLayout";
+import LoginLayoutRoute from "./layouts/LoginLayout";
+import CreateAccountPage from "./features/account/CreateAccountPage";
+import LoginPage from "./features/account/LoginPage";
+import TodoPage from "./features/todo/TodoPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <LoginLayoutRoute exact path="/create-account" component={CreateAccountPage} />
+        <LoginLayoutRoute
+          exact
+          path="/create-account"
+          component={CreateAccountPage}
+        />
         <LoginLayoutRoute exact path="/login" component={LoginPage} />
         <AppLayoutRoute exact path="/" component={TodoPage} />
       </Switch>
--- src/layouts/LoginLayout.js
+++ src/layouts/LoginLayout.js
@@ -1,19 +1,32 @@
 import "./LoginLayout.scss";
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Brand from "./Brand";
 
 import Notification from "../features/notifications/Notification";
+import { clearNotifications } from "../features/notifications/notificationsSlice";
 
-const LoginLayout = ({ children }) => (
-  <div className="login-layout">
-    <Notification />
-    <main className="page">
-      <Brand />
-      {children}
-    </main>
-  </div>
-);
+const LoginLayout = ({ children }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen((location) => {
+      dispatch(clearNotifications());
+    });
+  }, [dispatch, history]);
+
+  return (
+    <div className="login-layout">
+      <Notification />
+      <main className="page">
+        <Brand />
+        {children}
+      </main>
+    </div>
+  );
+};
 
 const LoginLayoutRoute = ({ component: Component, ...rest }) => {
   return (
