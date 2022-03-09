04dcd08beb12b1df3bc070633b1e8b6215abe442
Joshua Goss <josh@joshgoss.com>
2022-03-09 13:17:25 -0500

    various fixes

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,19 @@
+import {DateTime} from 'luxon';
 export {default as api} from './api';
+
+
 export const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-
 export const debounce = (callback, delay = 1500) => {
-    let timeoutId;
-    return async (...args) => {
-      clearTimeout(timeoutId);
-      return new Promise((resolve) => {
-        timeoutId = setTimeout(() => (
-          resolve(callback(...args))
-        ), delay);
-      });
-    };
+  let timeoutId;
+  return async (...args) => {
+    clearTimeout(timeoutId);
+    return new Promise((resolve) => {
+      timeoutId = setTimeout(() => (
+        resolve(callback(...args))
+      ), delay);
+    });
   };
+};
+
+export const prettyISODateTime =  (d) => (DateTime.fromISO(d).toLocaleString(DateTime.DATETIME_MED));
